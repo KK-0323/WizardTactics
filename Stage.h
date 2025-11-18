c7718df0fdec4b067a5cc9a09c0493cda271fe8c
今一憲 <240181@jc-21.jp>
2025-11-18 16:29:44 +0900

    Stageの変更をしたけどテクスチャの画像サイズが間違っているため表示されていない可能性がある

--- Stage.h
+++ Stage.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "Engine\\GameObject.h"
 #include "Engine\\Fbx.h"
-#include <vector>
-#include <fstream>
 
 class Stage :
     public GameObject
@@ -15,14 +13,6 @@ public:
     void Draw() override;
     void Release() override;
 private:
-    Fbx* pFbx_;
-    Fbx* blockFbx_;
-
-    static const int STAGE_WIDTH = 10;
-    static const int STAGE_HEIGHT = 5;
-
-    int mapData_[STAGE_HEIGHT][STAGE_WIDTH];
-
-    void LoadMapData(const std::string& filename);    
+    Fbx* pFbx_;    
 };
 
